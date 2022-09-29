dd148b62ca9c637070f381a16cd724a91b67933f
Chase <cnickellsumn@gmail.com>
2022-09-29 09:04:32 -0700

    EL FIN cleaned up code everything works
    Would like to find a 'reset all' function but that's it

--- rps-game.js
+++ rps-game.js
@@ -1,40 +1,31 @@
+// ROCK PAPER SCISSORS PROJECT
 
-
-
+// initial declarations
 const answers = ["rock", "paper", "scissors"]
 let result = ""
-
-function getComputerChoice() {
-    return answers[Math.floor(Math.random() * 3)];
-}
-
-const rBtn = document.querySelector(".rock")
-const pBtn = document.querySelector(".paper")
-const sBtn = document.querySelector(".scissors")
-const btns = document.querySelectorAll(".btn")
-let newUserChoice = ""
-
-btns.forEach(function (btn) {
-    btn.addEventListener('click', function (e) {
-        newUserChoice = e.currentTarget.id;
-        playRound(userChoice = newUserChoice)
-    })
-})
-
-// let pcChoice = getComputerChoice();
-// let userChoicePrompt = prompt("Rock, Paper, or Scissors?")
-// let userChoice = userChoicePrompt.toLowerCase()
-// chose to put this inside the round so that each
-// new round would prompt the user for a new answer
-
 let userScore = 0
 let pcScore = 0
 let round = 1
 
+// pulls randomly from answers[]
+function getComputerChoice() {
+    return answers[Math.floor(Math.random() * 3)];
+}
+
+// targeting buttons
+const btns = document.querySelectorAll(".btn")
+const restart = document.querySelector('.restart')
+
+// assigns listener per button to start round with that as userChoice
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+        playRound(userChoice = e.currentTarget.id)
+    })
+})
+
 function playRound () {  
-    let pcChoice = getComputerChoice();
-    // let userChoicePrompt = prompt("Rock, Paper, or Scissors?");
-    // let userChoice = userChoicePrompt.toLowerCase();          
+    // generate pcChoice and check against userChoice
+    let pcChoice = getComputerChoice();       
     
     if (userChoice === pcChoice) {
         result = "It's a tie!";
@@ -63,63 +54,47 @@ function playRound () {
             result = "Error: please enter a valid answer!";
             round--;
     }
-
-    alert(`--- Round ${round} of 5 ---` + "\n" + 
-        `User: ${userChoice}` + `\n` + 
-        `PC: ${pcChoice}` + `\n` + 
-        result + "\n" + "\n" + 
-        "Score:" + "\n" + 
-        `You: ${userScore}` + "\n" + 
-        `PC: ${pcScore}`);
-    
+    // display result and update score
+    document.querySelector('.result').textContent = result
+    checkScore();
 }
 
-function game() {
-    // using ROUND instead of i, this lets me modify
-    // the round within the PLAYROUND function
-    // incase user supplies an invalid answer
-    // and the round needs to be replayed
-    // LFG
-    for (round; round<6; round++) {
-        playRound();
+// if either score is 5, trigger gameOver otherwise update scores
+
+function checkScore() {
+    if (userScore === 5 || pcScore === 5) {
+        document.querySelector(".user-score").textContent = userScore
+        document.querySelector(".pc-score").textContent = pcScore
+        gameOver();
+    } else {
+        document.querySelector(".user-score").textContent = userScore
+        document.querySelector(".pc-score").textContent = pcScore
     }
-    alert("GAME OVER"
-    + "\n" + "\n" + 
-    "Score:" + "\n" + 
-    `You: ${userScore}` + "\n" + 
-    `PC: ${pcScore}`);            
 }
 
-       
+// hides original buttons and shows reset button
 
+function gameOver() {
+    document.querySelector('.title').textContent = "GAME OVER"
+    restart.classList.remove('hide') 
+    btns.forEach(function (urmom) {
+        urmom.classList.add('hide');
+    })        
+}
 
+// resets original text contents and scores
+// is there a straight up 'reset' method to refresh everything on page to default values?
 
-
-
-
-
-
-
-
-
-       // PLAY ROUND WORKS PERFECTLY (was missing brackets per "else" which added score
-       // when i didn't mean to)
-
-       // NOW TIME TO BUILD THE "FOR" LOOP TO PLAY THE GAME UP TO A SCORE OF 5, LFG
-       // goal for today's off session, FINISH the loop.  start the UI?
-
-       // i think i am starting to understand how this all wraps into web dev tho
-       // in real life i'd start using "document.querySelector(etc)" to target
-       // elements on the page and update them via my js script
-       // IT'S KINDA COOL NGL, it's becoming less fucking lame lmao
-
-
-        // PREVIOUS MID PROJECT NOTES:
-        // sooo for whatever reason when i pass in playRound(userChoice, pcChoice)
-        // IT DOESN'T FUCKING WORK, they are ALWAYS equal. makes no fucking sense tbh.
-        // but when I leave parameters blank it actually works
-        // is it because I'm calling them the same name as globally defined variables?
-        // idk
-
-        // using this reference
-        // https://www.geeksforgeeks.org/rock-paper-and-scissor-game-using-javascript/
+restart.addEventListener('click', function (e) {
+    document.querySelector('.title').textContent = "Choose Your Weapon"
+    restart.classList.add('hide') 
+    btns.forEach(function (urmom) {
+        urmom.classList.remove('hide');
+    }); 
+    result = "";
+    userScore = 0;
+    pcScore = 0;
+    document.querySelector('.result').textContent = result
+    document.querySelector(".user-score").textContent = userScore
+    document.querySelector(".pc-score").textContent = pcScore
+})
